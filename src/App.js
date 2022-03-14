3feae3370b6165ceeba6fa61c50976449e56e194
Andrey <geliosal39hon@gmail.com>
2022-03-14 20:44:56 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import {Form} from 'react-bootstrap'
+import {utils, read} from 'xlsx'
 
 function App() {
+
+  async function listenFile(e) {
+    const file = e.target.files[0]
+
+    const wb = read(await (await(file)).arrayBuffer(), {WTF: 1, cellDates: 1})
+
+    const data = utils.sheet_to_json(wb.Sheets[wb.SheetNames[0]], {defval: null})
+    console.log(data)
+
+    const date = new Date(data[0].date)
+     console.log(date.getDate());
+     console.log(date.getMonth());
+     console.log(date.getFullYear());
+     console.log(date.getTimezoneOffset());
+    
+    
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Form.Group controlId="formFile" className="mb-3">
+        <Form.Label>Выберете файл XLSX</Form.Label>
+        <Form.Control type="file" onChange={listenFile} />
+      </Form.Group>
     </div>
   );
 }
