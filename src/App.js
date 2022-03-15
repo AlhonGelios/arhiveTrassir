eb79d94fa92c2b444a5fe8f3f3bef45f8e73b6c6
Andrey <geliosal39hon@gmail.com>
2022-03-15 12:48:31 +0300

    15032022

--- src/App.js
+++ src/App.js
@@ -1,32 +1,11 @@
-import {Form} from 'react-bootstrap'
-import {utils, read} from 'xlsx'
+import InputArhiveFile from "./components/inputArhiveFile";
+import {Container} from 'react-bootstrap'
 
 function App() {
-
-  async function listenFile(e) {
-    const file = e.target.files[0]
-
-    const wb = read(await (await(file)).arrayBuffer(), {WTF: 1, cellDates: 1})
-
-    const data = utils.sheet_to_json(wb.Sheets[wb.SheetNames[0]], {defval: null})
-    console.log(data)
-
-    const date = new Date(data[0].date)
-     console.log(date.getDate());
-     console.log(date.getMonth());
-     console.log(date.getFullYear());
-     console.log(date.getTimezoneOffset());
-    
-    
-  }
-
   return (
-    <div className="App">
-      <Form.Group controlId="formFile" className="mb-3">
-        <Form.Label>Выберете файл XLSX</Form.Label>
-        <Form.Control type="file" onChange={listenFile} />
-      </Form.Group>
-    </div>
+    <Container>
+      <InputArhiveFile/>
+    </Container>
   );
 }
 
---
+++ src/components/inputArhiveFile.js
@@ -0,0 +1,67 @@
+import { useState } from 'react'
+
+import {Form} from 'react-bootstrap'
+import {utils, read} from 'xlsx'
+
+import getCamers from '../services/getCammers'
+
+function InputArhiveFile() {
+
+    const [records, setRecords] = useState([])
+
+    async function listenFile(e) {
+        const file = e.target.files[0]
+
+        const wb = read(await (await(file)).arrayBuffer(), {WTF: 1, cellDates: 1, FS: '_'})
+
+        const data = utils.sheet_to_json(wb.Sheets[wb.SheetNames[0]], {defval: null})
+        console.log(data)
+
+        const recordsOfTrassir = []
+
+        function addZero(item) {
+            if (item < 10) {
+                return item = '0' + item
+            } return item
+        }
+
+        data.forEach((item) => {
+
+            const date = new Date(item.date)
+            const dateUp = new Date(item.timeUp)
+            const dateDown = new Date(item.timeDown)
+            const timeUp = `${addZero(dateUp.getHours())}${addZero(dateUp.getMinutes())}`
+            const timeDown = `${addZero(dateDown.getHours())}${addZero(dateDown.getMinutes())}`
+            const day = addZero(date.getDate() + 1)
+            const month = addZero(date.getMonth() + 1)
+            const year = date.getFullYear()
+            const room = item.room
+
+            const fileName = `${item.type}_${item.speciality}_${item.stage}_${year}-${month}-${day}_${item.room}.avi`
+
+            const record = {
+                dateOfTrassirUp: `${year}${month}${day}_${timeUp}00`,
+                dateOfTrassirDown: `${year}${month}${day}_${timeDown}00`
+            }
+
+            recordsOfTrassir.push(record)
+        })
+
+        addRecords(recordsOfTrassir)
+    }
+
+    function addRecords(record) {
+        setRecords([...record])
+    }
+
+    return (
+      <div className="inputArhiveFile">
+        <Form.Group controlId="formFile" className="mb-3">
+          <Form.Label>Выберете файл XLSX</Form.Label>
+          <Form.Control type="file" onChange={listenFile} />
+        </Form.Group>
+      </div>
+    );
+  }
+
+  export default InputArhiveFile;
